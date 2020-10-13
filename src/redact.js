b345389e7c94217fef712544b00acfa8fb77b3bb
Renato <renatoargh@gmail.com>
2020-10-13 15:32:42 +0200

    PA-6438: [OPTIONAL] Trim non-JWT part of the match
    - The regex would match more than the token so the code in this commit will trim the beginning and the end until it makes sure only the JWT segment is redacted.
    - Example: before `token=jwt more stuff` would become `<<REDACTED>>`, now it will become `token=<<REDACTED>> more stuff`.
    - The cost are some more iterations, which will only happen when the logged string is in the format `[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*`.
    - Some non-jwt token in the same format as above: `www.example.com`, `v1.23.4`
    - If this additional performance cost is not acceptable we can revert this commit

--- src/redact.js
+++ src/redact.js
@@ -1,28 +1,55 @@
 const JWT_REGEX = /[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*/
 const REDACT_TEXT = '<<REDACTED JWT>>'
 
+const fromBase64 = (value) => Buffer.from(value, 'base64').toString('ascii')
+
 const isRegExp = value => value instanceof RegExp
 const isDate = value => value instanceof Date
 const isString = value => typeof value === 'string'
 const isArray = value => Array.isArray(value)
 const isObject = value => typeof value === 'object'
-const isJWT = (value) => {
-  const [, payload] = value.split('.')
+
+const isJWTStart = (value) => {
+  const [headers, payload] = value.split('.')
 
   try {
-    const decodedPayload = Buffer.from(payload, 'base64').toString()
-    const { iat, exp } = JSON.parse(decodedPayload)
+    // Jwt headers and payload must be valid JSON
+    JSON.parse(fromBase64(payload))
+    JSON.parse(fromBase64(headers))
 
-    return !!iat && !!exp
+    return true
   } catch (err) {
     return false
   }
 }
 
-const replaceJwtOccurrences = message => message.replace(
-  JWT_REGEX,
-  occurrence => (isJWT(occurrence) ? REDACT_TEXT : occurrence),
-)
+const isJWTEnd = (value) => {
+  const [,, signature] = value.split('.')
+
+  try {
+    // A JWT signature is an HMAC-SHA256 hash, so the only
+    // validation here is to check if it consists of 256 bytes
+    return Buffer.from(fromBase64(signature)).length === 256
+  } catch (err) {
+    return false
+  }
+}
+
+const replaceJwtOccurrences = message => message.replace(JWT_REGEX, occurrence => {
+  let actualJwt = occurrence
+  
+  while(!isJWTStart(actualJwt) && actualJwt.length) {
+    actualJwt = actualJwt.substring(1)
+  }
+  
+  while(!isJWTEnd(actualJwt) && actualJwt.length) {
+    actualJwt = actualJwt.slice(0, -1)
+  }      
+  
+  // If we have a match, then `actualJwt` will contain the 
+  // jwt token, otherwise we end up with an empty string
+  return actualJwt ? occurrence.replace(actualJwt, REDACT_TEXT) : occurrence
+})
 
 const redactSensitiveData = (message) => {
   if (isString(message)) {
