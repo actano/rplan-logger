92b8c8e5fd7b828d6385079a81eeadb473ef9a29
Neha Pednekar <neha.pednekar@gmail.com>
2020-10-13 12:42:39 +0200

    PA-6438: Redact JWT tokens from logs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import BunyanLogger from 'bunyan'
 
 import config from './config'
+import { redact } from './redact'
 
 class Logger extends BunyanLogger {
   withNamespace(namespace) {
     const parentNamespace = this.fields.namespace
     const childNamespace = parentNamespace ? `${parentNamespace}.${namespace}` : namespace
     return this.child({ namespace: childNamespace })
+  }
+
+  debug(...args) {
+    super.debug(...redact(args))
+  }
+
+  error(...args) {
+    super.error(...redact(args))
+  }
+
+  info(...args) {
+    super.info(...redact(args))
+  }
+
+  trace(...args) {
+    super.trace(...redact(args))
+  }
+
+  warn(...args) {
+    super.warn(...redact(args))
   }
 }
 
